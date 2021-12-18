7b0e7f821317a52bd8cfd069d789be8b9664a380
Sanjith <85774398+Sanjith7146@users.noreply.github.com>
2021-12-18 19:15:46 +0800

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by sanjithsethu on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
